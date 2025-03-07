96eecc9c5c8951bd002c0de7ec6fc12b8c7a09ff
Logan Waite <lwaite2010@gmail.com>
2025-03-07 07:28:37 -0500

    try relative path

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from generator_utils import copy_directory, generate_page_recursively
 
 
 def main():
-    basepath = sys.argv[1]
+    basepath = sys.argv[1] if len(sys.argv) == 2 else ""
     copy_directory('static', 'docs')
     generate_page_recursively(
         'content', 'template.html', 'docs', basepath)
